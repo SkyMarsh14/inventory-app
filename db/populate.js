072e8da3b706309119eee7d8df2f033c879d667b
Sky Randy <soras1758@gmail.com>
2025-03-08 18:03:41 +0900

    add file to populate db.

---
+++ db/populate.js
@@ -0,0 +1,74 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+const { argv } = require("node:process");
+
+const DEVICE = `CREATE TABLE IF NOT EXISTS devices (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR (255),
+    brandId INTEGER,
+    category VARCHAR (255),
+    FOREIGN KEY (brandId) REFERENCES brands(id)
+    )`;
+
+const DEVICE_VALUES = `
+    INSERT INTO devices (name,brandId,category)
+    VALUES
+    ('M4 Macbook Air',1,'laptop'),
+    ('iPhone 16 Pro',1,'smartphone'),
+    ('XPS 14',3,'laptop'),
+    ('Galaxy S25',2,'smartphone'),
+    ('Galaxy Book5 Pro',2 ,'laptop')
+    `;
+
+const SUPPLIER = `CREATE TABLE IF NOT EXISTS suppliers (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    supplierName VARCHAR (255)
+);
+
+INSERT INTO suppliers (supplierName)
+VALUES 
+    ('Amazon'),
+    ('Best Buy'),
+    ('Maker Direct');`;
+
+const BRAND = `CREATE TABLE IF NOT EXISTS brands (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    brandName VARCHAR (255)
+);
+INSERT INTO brands (brandName)
+VALUES 
+    ('Apple'),
+    ('Samsung'),
+    ('Dell');
+`;
+
+const DEVICE_SUPPLIER = `CREATE TABLE IF NOT EXISTS device_suppliers (
+    deviceId INT,
+    supplierId INT,
+    PRIMARY KEY (deviceId, supplierId),
+    FOREIGN KEY (deviceId) REFERENCES devices(id),
+    FOREIGN KEY (supplierId) REFERENCES suppliers(id)
+)`;
+
+async function main() {
+  console.log("Seeding...");
+  const client = new Client({
+    connectionString: argv[2],
+  });
+  try {
+    await client.connect();
+    await client.query(BRAND);
+    await client.query(DEVICE);
+    await client.query(SUPPLIER);
+    await client.query(DEVICE_SUPPLIER);
+    await client.query(DEVICE_VALUES);
+    console.log("Seeding completed!");
+  } catch (err) {
+    console.error("Error during seeding:", err);
+  } finally {
+    await client.end();
+  }
+}
+
+main();
