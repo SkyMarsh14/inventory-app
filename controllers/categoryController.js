92103a58df15a801b695736e6ebb9d442de268fc
Sky Randy <soras1758@gmail.com>
2025-03-09 13:17:43 +0900

    add error handling for category form.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,11 +1,35 @@
 const queries = require("./../db/queries");
+const { body, validationResult } = require("express-validator");
 
+const validateCategory = [
+  body("category")
+    .trim()
+    .notEmpty()
+    .withMessage("Ths field is required.")
+    .isAlpha()
+    .withMessage("Must only contain letters.")
+    .isLength({ min: 5, max: 25 })
+    .withMessage("Category name must be between 5 to 25 letters."),
+];
 const controller = {
   get: async (req, res) => {
     const categories = await queries.getCategories();
     res.render("categories", { categories });
   },
-  post: async (req, res) => {},
+  post: [
+    validateCategory,
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const categories = await queries.getCategories();
+        return res
+          .status(400)
+          .render("categories", { categories, errors: errors.array() });
+      }
+      const { category } = req.body;
+      res.redirect("/");
+    },
+  ],
 };
 
 module.exports = controller;
