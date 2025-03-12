878c35aab9246b0bc91013c540122f27dd68f9e9
Sky Randy <soras1758@gmail.com>
2025-03-12 13:50:01 +0900

    refactored device-list view to table

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,7 +2,7 @@ const queries = require("./../db/queries");
 
 const indexController = async (req, res) => {
   const devices = await queries.getAllDevices();
-  res.render("index", { devices });
+  res.render("index", { data: devices });
 };
 
 module.exports = indexController;
