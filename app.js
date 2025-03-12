663d2e5de5978445b43ded1f54c58d7d000a4012
Sky Randy <soras1758@gmail.com>
2025-03-12 16:35:28 +0900

    add basic stylings

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use("/", indexRouter);
 app.use("/category", categoryRouter);
